5849de32d28912d6991acc72523a16a70011dfee
Ralf <ralf@stro-bel.de>
2018-10-03 22:42:56 +0200

    init

---
+++ precache-manifest.5459760b5811ff2da1a443adbb87efe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "411c684815fbf33ed5c9",
+    "url": "/pwa-test/static/js/runtime~main.411c6848.js"
+  },
+  {
+    "revision": "c74d07fdc9e6481b0534",
+    "url": "/pwa-test/static/js/main.c74d07fd.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/pwa-test/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c74d07fdc9e6481b0534",
+    "url": "/pwa-test/static/css/main.f0d43649.chunk.css"
+  },
+  {
+    "revision": "cf3bc0f09cc9a07c20fc98a741d8b529",
+    "url": "/pwa-test/index.html"
+  }
+];
